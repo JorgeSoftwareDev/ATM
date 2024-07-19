eb1f8002e093db17038437679a1617f0ccb6d4a6
Jorge Alvarado <111322460+JorgeSoftwareDev@users.noreply.github.com>
2024-07-18 18:00:16 -0700

    creating repository

---
+++ ATM/src/Users.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Users {
+	// First name of the account holder
+		private String fName;
+		
+		// Last name of the account holder
+		private String lName;
+		
+		// Unique Identifier of the account holder
+		private String uuid;
+		
+		// Has of the user's PIN number
+		private byte pinHash[];
+		
+		// The List of accounts for this user
+		private ArrayList<String> accounts;
+		
+}
